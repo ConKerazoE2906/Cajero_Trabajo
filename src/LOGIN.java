19ca2add7198e68fdfa473ac5f25154deab50041
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-02-05 14:10:33 -0500

    version tres

--- src/LOGIN.java
+++ src/LOGIN.java
@@ -17,22 +17,126 @@ public class LOGIN extends JFrame {
     private JButton boton_cero;
     private JLabel texto;
     private JLabel numeros_pantalla;
+    private JButton boton_salir;
 
     public LOGIN() {
+
         boton_cero.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                /*String texto_pantalla = numeros_pantalla.getText();
-                String valores = numeros_pantalla + "0";
-                numeros_pantalla.setText(valores);*/
+                String numero="0";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
             }
         });
         boton_uno.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                /*String texto_pantalla = numeros_pantalla.getText();
-                String valores = numeros_pantalla + "1";
-                numeros_pantalla.setText(valores);*/
+                String numero="1";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
+            }
+        });
+        boton_dos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero="2";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
+            }
+        });
+        boton_tres.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero="3";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
+            }
+        });
+
+        boton_cuatro.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero="4";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
+            }
+        });
+        boton_cinco.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero="5";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
+            }
+        });
+        boton_seis.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero="6";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
+            }
+        });
+        boton_siete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero="7";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
+            }
+        });
+        boton_ocho.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero="8";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
+            }
+        });
+        boton_nueve.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero="9";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla + numero;
+                numeros_pantalla.setText(valores);
+            }
+        });
+        boton_salir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main.frame.dispose();
+            }
+        });
+
+
+        boton_validar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String clave_ingreso = "122928";
+                String texto_pantalla = numeros_pantalla.getText();
+                String valores = texto_pantalla;
+
+                if (clave_ingreso.equals(valores)){
+                    System.out.println("hola");
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "CREDENCIALES INCORRECTAS");
+                    Main.frame.dispose();
+                    System.out.println("adios");
+
+                }
             }
         });
     }
--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
     static JFrame frame = new JFrame();
     public static void main(String[] args) {
 
-        /*frame.setUndecorated (true);
+        frame.setUndecorated (true);
         frame.setContentPane(new LOGIN().panel_login);
         frame.setSize(400,200);
         frame.setLocationRelativeTo(null);
         frame.pack();
-        frame.setVisible(true);*/
+        frame.setVisible(true);
 
 
     }
